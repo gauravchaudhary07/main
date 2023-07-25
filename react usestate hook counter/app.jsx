516ab1d2f5180f63d6feff921420cdc09c0b29da
Gaurav Chaudhary <103997827+gauravchaudhary07@users.noreply.github.com>
2023-07-25 11:33:43 +0530

    Create app.jsx

---
+++ react usestate hook counter/app.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+     class Counter extends React.Component {
+        constructor(props) {
+           super(props)
+          this.state = {
+            count: 0,
+                     }
+           }
+    increment () ⇒ {
+    this.setState(prevState ⇒({ count: prevState.count + 1 }))
+    }
+    decrement () ⇒ {
+    this.setState(prevState → ({ count: prevState.count - 1 }))
+    }
+  render() {
+      return (
+            <div>
+            <button onClick={this. increment}>Click to increment</button> 
+            <button onClick={this.decrement} >Click to decrement</button> <p>{this.state.count}</p>
+            </div>
+         )
+       }
+    }
